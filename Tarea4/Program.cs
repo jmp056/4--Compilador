576263b9be5fe68e67f9b6a79e5b92fde11f3ddd
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-06-25 12:41:33 -0400

    Funcion de la sintaxis terminada!

---
+++ Tarea4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite la linea que desea interpretar: ");
+            string Comando = Console.ReadLine();
+
+            if (!Sintactica(Comando))
+                Console.WriteLine("La línea de comando posee un error de sintaxis.");
+
+
+
+
+            Console.ReadKey();
+        }
+
+        public static bool Sintactica(string Comando)
+        {
+            bool paso = false;
+            string Ultimo = Comando.Substring(Comando.Length - 1, 1);
+
+            if (Ultimo == ";")
+                paso = true;
+
+            return paso;
+        }
+    }
+}
