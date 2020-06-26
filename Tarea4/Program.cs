695c769e29803521d49969323bac1fc651405b71
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-06-25 21:30:42 -0400

    funcion lexicografica  terminada!

--- Tarea4/Program.cs
+++ Tarea4/Program.cs
@@ -13,15 +13,60 @@ namespace Tarea4
             Console.WriteLine("Digite la linea que desea interpretar: ");
             string Comando = Console.ReadLine();
 
-            if (!Sintactica(Comando))
-                Console.WriteLine("La línea de comando posee un error de sintaxis.");
-
-
-
+            if(!Lexicografia(Comando))
+                Console.WriteLine("La línea de comando posee un error lexicográfico!");
+            
+            //if (!Sintactica(Comando))
+            //    Console.WriteLine("La línea de comando posee un error de sintaxis!");
 
             Console.ReadKey();
+
+
+
+
+            //string datos = "hola esto es un ejemplo para extraer";
+            //string[] datoscortados = data.Split(' ');
+            //MessageBox.Show(datoscortados[1]);
+
         }
 
+        public static bool Lexicografia(string Comando)
+        {
+            bool paso = false;
+
+            string declaracion = " ";
+            int contador = 0;
+
+            while(Comando[contador] != ' ')
+            {
+                declaracion += Comando[contador];
+                contador++;
+            }
+
+            if (PalabrasReservadas(declaracion) == true)
+                paso = true;
+
+
+            return paso;
+        }
+
+        public static bool PalabrasReservadas(string declaracion)
+        {
+            bool paso = false;
+            declaracion = declaracion.Trim();
+
+            if (declaracion == "entero" ||
+               declaracion == "flotante" ||
+               declaracion == "letra" ||
+               declaracion == "palabra" ||
+               declaracion == "oracion" ||
+               declaracion == "booleano")
+                
+                paso = true;
+
+            return paso;
+        }
+        
         public static bool Sintactica(string Comando)
         {
             bool paso = false;
